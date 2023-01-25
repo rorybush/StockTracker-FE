6062e565f3fedd87ea9d061578e7c349700d317e
Tayamul <rai_tayamul@hotmail.com>
2023-01-25 13:18:40 +0000

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import {CssBaseline} from '@mui/material'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline/>
+      <NavBar/>
+      <Home/>
+      <Routes>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+      </Routes>
     </div>
   );
 }
