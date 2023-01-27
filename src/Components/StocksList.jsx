21954259320127b4034f28d84a8194306e8656b0
Tayamul <rai_tayamul@hotmail.com>
2023-01-27 15:11:01 +0000

    Get list of ftse100 stocks

--- src/Components/StocksList.jsx
+++ src/Components/StocksList.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import * as api from '../utils/api'
 
 const StocksList = () => {
 
     const [stocksList, setStocksList] = useState([])
 
     useEffect(() => {
-
+      api.getStockListFtse().then((data) => {
+        setStocksList(data)
+      })
     }, [])
   return (
-    <div>StocksList</div>
+    <div>
+      <ul>
+        {stocksList.map((stock) => {
+          return (
+            <li key={stock.symbol}>
+              <h5>{stock.companyName}</h5>
+              <h6>{stock.symbol}</h6>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
    
   )
 }
