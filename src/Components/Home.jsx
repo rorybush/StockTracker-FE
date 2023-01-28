4d9bff5a649acfae5d5b49270ba5030191da33e6
yusuf <majorhaji@github.com>
2023-01-28 23:52:04 +0000

    disclaimer done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,5 @@
 import { Container, Grid } from "@mui/material";
 import React from "react";
-import StockCalendar from "./StockCalendar";
 import StocksList from "./StocksList";
 import TickerList from "./TickerList";
 // import StockNews from './StockNews'
@@ -11,7 +10,6 @@ const Home = () => {
       <Grid container spacing={2}>
         <Grid item>
           <TickerList />
-          <StockCalendar />
           {/* <StockNews/> */}
           {/* <StocksList /> */}
         </Grid>
