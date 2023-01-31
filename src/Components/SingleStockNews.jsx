9e9fe8342a807cad63f21c859e5548cb74bb925d
Tayamul <rai_tayamul@hotmail.com>
2023-01-31 22:51:30 +0000

    Add responsiveness to the grid container of the stock news

--- src/Components/SingleStockNews.jsx
+++ src/Components/SingleStockNews.jsx
@@ -10,108 +10,130 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  CircularProgress
 } from "@mui/material";
 import moment from "moment";
 
+const SingleStockNews = ({ symbol }) => {
+  const [singleStockNews, setSingleStockNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    setIsLoading(true);
+    api
+      .getSingleStockNews(symbol)
+      .then((data) => {
+        setSingleStockNews(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-const SingleStockNews = ({symbol}) => {
+  if (isLoading)
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
 
-    const [singleStockNews, setSingleStockNews] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-      setIsLoading(true)
-        api.getSingleStockNews(symbol).then((data) => {
-            setSingleStockNews(data)
-            setIsLoading(false)
-        }).catch(err => {
-            console.log(err)
-        })
-    },[])
-
-    if(isLoading) return <p>Loading...</p>
-    
-   return (
-    
-    <Container maxWidth="md" sx={{ padding: "35px 20px", width: 320 }}>
-      <Grid container spacing={0.5}>
-        {singleStockNews.length > 0 && singleStockNews.map((news) => (
-          <Grid item key={news.uuid}>
-            <Card sx={{ display: "flex" }}>
-              <CardContent
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  width:320
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 40,
-                    width: 320,
-                  }}
-                >
-                   <CardActionArea href={news.link} target="_blank">
-                  <Stack
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        padding: "35px 20px",
+        width: { lg: 500, md: 450, sm: 400, xs: 350 },
+        mr: { lg: 8, md: 7, sm: 6 },
+      }}
+    >
+      {<p style={{ color: "#0288D1" }}>Top Stories</p>}
+      <Grid container spacing={3}>
+        {singleStockNews.length > 0 &&
+          singleStockNews.map((news) => (
+            <Grid item key={news.uuid}>
+              <Card sx={{ display: "flex" }}>
+                <CardActionArea href={news.link} target="_blank">
+                  <CardContent
                     sx={{
                       display: "flex",
                       flexDirection: "row",
-                      justifyContent: "space-between",
-                      mr: 1,
+                      width: { lg: 500, md: 450, sm: 400, xs: 350 },
+                      height: { lg: 100, md: 100, sm: 100, xs: 100 },
                     }}
                   >
-                   
-                    <Typography sx={{fontSize:9}} color="text.secondary">
-                      {news.publisher}
-                    </Typography>
-                    <Typography sx={{fontSize:8}} color="text.secondary">
-                      {moment(news.providerPublishTime * 1000).fromNow()}
-                    </Typography>
-                  </Stack>
-                  <Typography sx={{fontSize:10}}>{news.title}</Typography>
-                  
-                    <Typography sx={{fontSize:8}} color="text.secondary">
-                      Link to news article
-                    </Typography>
-                  </CardActionArea>
-                </Box>
-                {news.hasOwnProperty("thumbnail") ? (
-                  <CardMedia
-                    component="img"
-                    image={news.thumbnail.resolutions[0].url}
-                    sx={{
-                      // paddingTop: "56.25%",
-                      height: 60,
-                      width: 60,
-                      borderRadius: "10%",
-                      ml: 2,
-                    }}
-                  />
-                ) : (
-                  // image={`https://source.unsplash.com/random?${article.title}`}
-                  <CardMedia
-                    component="img"
-                    image={`https://source.unsplash.com/random?${news.publisher} `}
-                    sx={{
-                      // paddingTop: "56.25%",
-                      height: 60,
-                      width: 60,
-                      borderRadius: "10%",
-                      ml: 2,
-                      mb: 1,
-                    }}
-                  />
-                )}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        height: { lg: 40, md: 40, sm: 45, xs: 60 },
+                        width: { lg: 400, md: 350, sm: 300, xs: 250 },
+                      }}
+                    >
+                      <Stack
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          mr: 1,
+                        }}
+                      >
+                        <Typography
+                          sx={{ fontSize: 11 }}
+                          color="text.secondary"
+                        >
+                          {news.publisher}
+                        </Typography>
+                        <Typography
+                          sx={{ fontSize: 10 }}
+                          color="text.secondary"
+                        >
+                          {moment(news.providerPublishTime * 1000).fromNow()}
+                        </Typography>
+                      </Stack>
+                      <Typography sx={{ fontSize: 12 }}>
+                        {news.title}
+                      </Typography>
+
+                      <Typography sx={{ fontSize: 10 }} color="text.secondary">
+                        Link to news article
+                      </Typography>
+                    </Box>
+                    {news.hasOwnProperty("thumbnail") ? (
+                      <CardMedia
+                        component="img"
+                        image={news.thumbnail.resolutions[0].url}
+                        sx={{
+                          // paddingTop: "56.25%",
+                          height: 60,
+                          width: 60,
+                          borderRadius: "10%",
+                          ml: 2,
+                        }}
+                      />
+                    ) : (
+                      // image={`https://source.unsplash.com/random?${article.title}`}
+                      <CardMedia
+                        component="img"
+                        image={`https://source.unsplash.com/random?${news.publisher} `}
+                        sx={{
+                          // paddingTop: "56.25%",
+                          height: 60,
+                          width: 60,
+                          borderRadius: "10%",
+                          ml: 2,
+                          mb: 1,
+                        }}
+                      />
+                    )}
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
       </Grid>
     </Container>
   );
-}
+};
 
-export default SingleStockNews
+export default SingleStockNews;
