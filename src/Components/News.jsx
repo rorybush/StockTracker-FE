dd518335d5d991b273a4c3af59d64eaebc06aedd
Tayamul <rai_tayamul@hotmail.com>
2023-01-31 09:15:12 +0000

    Change styling to the news component

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -18,90 +18,94 @@ const StockNews = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setIsLoading(true)
-    api.getStockNews().then((data) => {
-      setStockNews(data);
-      setIsLoading(false);
-    }).catch(err => {
-      console.log(err)
-    })
+    setIsLoading(true);
+    api
+      .getStockNews()
+      .then((data) => {
+        setStockNews(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
-  if(isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
   return (
-    <Container maxWidth="lg" sx={{ padding: "35px 20px", margin:"0 auto" }}>
-      <Grid container spacing={3} >
+    <Container
+      maxWidth="lg"
+      sx={{ padding: "35px 20px", margin: "0 auto"}}
+    >
+      <Grid container spacing={3}>
         {stockNews.map((news) => (
-          <Grid item key={news.uuid} >
+          <Grid item key={news.uuid} xs={12} sm={6} md={4} >
             <Card sx={{ display: "flex"}}>
-              <CardContent
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  width:550,
-                  height:400              
-                }}
-              >
-                 {news.hasOwnProperty("thumbnail") ? (
-                  <CardMedia
-                    component="img"
-                    image={news.thumbnail.resolutions[0].url}
-                    sx={{
-                      // paddingTop: "56.25%",
-                      height: 330,
-                      width: 500,
-                      borderRadius: "2%",
-                      mb: 1,
-                    }}
-                  />
-                ) : (
-                  // image={`https://source.unsplash.com/random?${article.title}`}
-                  <CardMedia
-                    component="img"
-                    image={`https://source.unsplash.com/random?${news.publisher} `}
-                    sx={{
-                      // paddingTop: "56.25%",
-                      height: 330,
-                      width: 500,
-                      borderRadius: "2%",
-                      mb: 1,
-                    }}
-                  />
-                )}
-                <Box
+              <CardActionArea href={news.link} target="_blank">
+                <CardContent
                   sx={{
                     display: "flex",
                     flexDirection: "column",
-                    height: 50,
-                    width: 500,
+                    width: 340,
+                    height: 380
                   }}
                 >
-                  <Stack
+                  {news.hasOwnProperty("thumbnail") ? (
+                    <CardMedia
+                      component="img"
+                      image={news.thumbnail.resolutions[0].url}
+                      sx={{
+                        // paddingTop: "56.25%",
+                        width: 315,
+                        height: 250,
+                        borderRadius: "2%",
+                        mb: 1,
+                      }}
+                    />
+                  ) : (
+                    <CardMedia
+                      component="img"
+                      image={`https://source.unsplash.com/random?${news.publisher}`}
+                      sx={{
+                        width: 315,
+                        height: 250,
+                        borderRadius: "2%",
+                        mb: 1,
+                      }}
+                    />
+                  )}
+                  <Box
                     sx={{
                       display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      mr: 1,
-                      ml:1,
+                      flexDirection: "column",
+                      width: 325,
+                      height: 30,
                     }}
                   >
-                    <Typography sx={{fontSize:12}} color="text.secondary">
-                      {news.publisher}
-                    </Typography>
-                    <Typography sx={{fontSize:11}} color="text.secondary">
-                      {moment(news.providerPublishTime * 1000).fromNow()}
-                    </Typography>
-                  </Stack>
-                  <Typography sx={{fontSize:14}}>{news.title}</Typography>
-                  <CardActionArea href={news.link} target="_blank">
-                    <Typography sx={{fontSize:11}} color="text.secondary">
+                    <Stack
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        mr: 1,
+                        ml: 1,
+                      }}
+                    >
+                      <Typography sx={{ fontSize: 12 }} color="text.secondary">
+                        {news.publisher}
+                      </Typography>
+                      <Typography sx={{ fontSize: 11 }} color="text.secondary">
+                        {moment(news.providerPublishTime * 1000).fromNow()}
+                      </Typography>
+                    </Stack>
+                    <Typography sx={{ fontSize: 14 }}>{news.title}</Typography>
+
+                    <Typography sx={{ fontSize: 11 }} color="text.secondary">
                       Link to news article
                     </Typography>
-                  </CardActionArea>
-                </Box>
-               
-              </CardContent>
+                  </Box>
+                </CardContent>
+              </CardActionArea>
             </Card>
           </Grid>
         ))}
@@ -111,4 +115,3 @@ const StockNews = () => {
 };
 
 export default StockNews;
-
